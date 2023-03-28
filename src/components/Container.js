1009ee71f9e6d446da090c66b20833aff1dff920
Emily <easchoettler@gmail.com>
2023-03-28 16:51:06 -0400

    Create game functionality

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,57 +1,74 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/Container.css'
-import Card from './Card'
-import dog1 from '../assets/dog1.jpg'
-import dog2 from '../assets/dog2.jpg'
-import dog3 from '../assets/dog3.jpg'
-import dog4 from '../assets/dog4.jpg'
-import dog5 from '../assets/dog5.jpg'
-import dog6 from '../assets/dog6.jpg'
-import dog7 from '../assets/dog7.jpg'
-import dog8 from '../assets/dog8.jpg'
-import dog9 from '../assets/dog9.jpg'
-import dog10 from '../assets/dog10.jpg'
-import dog11 from '../assets/dog11.jpg'
-import dog12 from '../assets/dog12.jpg'
 import uniqid from 'uniqid'
 
 const Container = (props) => {
-    const { setScore, bestScore, setBestScore } = props
+    const { score, setScore, bestScore, setBestScore } = props
 
-    const handleClick = () => {
-        setCardArr(prevCardArr => shuffleArray([...prevCardArr]))
+    // Import all dog images
+    function importAll(r) {
+        let images = {};
+        r.keys().forEach((item) => { images[item.replace('./', '')] = r(item); });
+        return images;
+      }
+    const images = importAll(require.context('../assets/dogs', false, /\.(png|jpe?g|svg)$/));
+
+    // Create initial adjacency list & set state
+    let initialArr = []
+    for (let i=1; i<13; i++) {
+        initialArr.push({id: i, status: 'unclicked'})
+    }
+    const [arr, setArr] = useState(initialArr)
+
+    const resetRound = () => {
+        // Reset round score
+        setScore(0)
+        // Change all images back to unclicked
+        setArr(prevArr => {
+            return prevArr.map(item => {
+                return {...item, status: 'unclicked'}
+            })
+        })
+    }
+    
+    const handleClick = (e) => {        
+        // If an image hasn't been clicked
+        if ((e.target.classList.contains('unclicked'))) {
+            // Increase the round score
+            setScore(prevScore => prevScore + 1)
+            // Mark the targeted image as clicked
+            setArr(prevArr => {
+                return shuffleArray(prevArr.map(item => {
+                    return item.id === parseInt(e.target.id) ? {...item, status: 'clicked'} : item
+                }))
+            })
+        } else {
+            resetRound()
+        }
     }
 
-    const [cardArr, setCardArr] = useState(
-        [
-            <Card location={dog1} alt='' key={uniqid()} handleClick={handleClick}/>,
-            <Card location={dog2} alt='' className='crop-right' key={uniqid()} handleClick={handleClick}/>,
-            <Card location={dog3} alt='' key={uniqid()} handleClick={handleClick}/>,
-            <Card location={dog4} alt='' className='crop-right' key={uniqid()} handleClick={handleClick}/>,
-            <Card location={dog5} alt='' key={uniqid()} handleClick={handleClick}/>,
-            <Card location={dog6} alt='' key={uniqid()} handleClick={handleClick}/>,
-            <Card location={dog7} alt='' key={uniqid()} handleClick={handleClick}/>,
-            <Card location={dog8} alt='' key={uniqid()} handleClick={handleClick}/>,
-            <Card location={dog9} alt='' key={uniqid()} handleClick={handleClick}/>,
-            <Card location={dog10} alt='' className='crop-left' key={uniqid()} handleClick={handleClick}/>,
-            <Card location={dog11} alt='' className='crop-left' key={uniqid()} handleClick={handleClick}/>,
-            <Card location={dog12} alt='' key={uniqid()} handleClick={handleClick}/>
-        ]
-    )
+    // If round score is higher than best score, update best score
+    useEffect(() => { if (score > bestScore) setBestScore(score) }, [score])
 
     const shuffleArray = (array) => {
-        console.log("shuffling")
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
         return array
     }
-    
+
     return(
         <div className='container'>
-            {cardArr.map(card => {
-                return card;
+            {arr.map(item => {
+                return <img 
+                    src={images[`dog${item.id}.jpg`]} 
+                    alt='Picure of a dog running' 
+                    id={item.id} 
+                    key={uniqid()} 
+                    className={item.status} 
+                    onClick={handleClick}>
+                </img>
             })}
         </div>
     )
